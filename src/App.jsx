44ee3cf9a87af88e00d42b47c7acf74cea77df2e
ChuckWeindorf <charles.weindorf@mudsox.com>
2024-02-06 15:59:32 -0500

    Function app in react

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,44 @@
+import './App.css'
+import FilmsList from './components/filmslist'
+import {useState} from "react";
+
+function App(props)
+ {
+const [list2, setList] = useState(["ready","set","go"]);
+const [text, setText] = useState("");
+
+
+  function onSubmit2(event){ 
+    {
+    event.preventDefault();  
+    let list2 = [...list2, text];
+    setList({list2: list2, text: ""});
+  }
+  }
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <form onSubmit={onSubmit2}>
+          <input 
+          type="text" 
+          id="todoInput"
+          name="todoInput"
+          value={text}
+          onChange={(changeEvent) => {
+            setText({text: changeEvent.target.value});
+          }}
+          />
+          <button type="submit">Add</button>
+        </form>  
+        <ul>{list2.map((element,index) => {return <li key={element+index}>{element}</li>})}</ul>
+      <FilmsList />
+      </div>
+    )
+  }
+
+//prior version as class app
+/*
 import React, {Component} from "react";
-//import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
 import './App.css'
 import FilmsList from './components/filmslist'
 
@@ -45,7 +83,7 @@ class App extends React.Component {
     )
   }
 }
-
+*/
 //prior version from lesson intro to react
 /*function App() {
   const [count, setCount] = useState(0)
--- src/components/filmslist.jsx
+++ src/components/filmslist.jsx
@@ -1,39 +1,67 @@
-import React, {Component} from "react";
+import { useState, useEffect } from "react";
 
+export default function FilmsList(props) {
+  let [listNew, setList] = useState([]);
+  useEffect(() => {
+    getFilms([]);
+  }, []);
 
-class FilmsList extends React.Component{
-    constructor(props)
-        {    
-            super(props)
-            this.state = 
-                {
-                list: []
-                }
-        }
+  function getFilms() {
+    console.log("Get Films now");
+    fetch(`https://studioghibliapi-d6fc8.web.app/films`)
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setList(data);
+      })
+      .catch(console.error);
+  }
 
-    getFilms(){
-        console.log("Get Films now");
-        fetch(`https://studioghibliapi-d6fc8.web.app/films`)
-        .then((response) => {
-                           console.log(response);
-                           return response.json()
-                          })
-           .then((data) => {console.log(data);
-                            this.setState({list: data})})
-        .catch(console.error);
-     }
+  return (
+    <ul>
+      {listNew.map((film) => {
+        return <li key={film.id}>{film.title}</li>;
+      })}
+    </ul>
+  );
+}
 
-     componentDidMount(){this.getFilms()}
-
-     render() {
-        return (
-        <ul>
-        {this.state.list.map(element => {
-          return <li key={element.id}>{element.title}</li>
-          })}
-        </ul>
-        )
-      }        
-    }
+//class version
+/*
+    class FilmsList extends React.Component{
+      constructor(props)
+          {    
+              super(props)
+              this.state = 
+                  {
+                  list: []
+                  }
+          }
   
-    export default  FilmsList;
+      getFilms(){
+          console.log("Get Films now");
+          fetch(`https://studioghibliapi-d6fc8.web.app/films`)
+          .then((response) => {
+                             console.log(response);
+                             return response.json()
+                            })
+             .then((data) => {console.log(data);
+                              this.setState({list: data})})
+          .catch(console.error);
+       }
+  
+       componentDidMount(){this.getFilms()}
+  
+       render() {
+          return (
+          <ul>
+          {this.state.list.map(element => {
+            return <li key={element.id}>{element.title}</li>
+            })}
+          </ul>
+          )
+        }        
+      }*/
